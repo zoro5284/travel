feb8163756093230db9742e96ff8b1e4ce76821a
zoro5284 <577270884@qq.com>
2020-06-18 20:11:30 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,109 @@
+<template>
+    <div>
+        <div class="recommend-title">热销推荐</div>
+        <ul>
+            <li
+                class="item border-bottom"
+                v-for="item in recommendList"
+                :key="item.id"
+            >
+                <img class="item-img" :src="item.imgUrl" />
+                <div class="item-info">
+                    <p class="item-title">{{ item.title }}</p>
+                    <p class="item-desc">
+                        {{ item.desc }}
+                    </p>
+                    <button class="item-button">查看详情</button>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeRecommend',
+    data () {
+        return {
+            recommendList: [
+                {
+                    id: '0001',
+                    imgUrl:
+                        '//imgs.qunarzz.com/p/p29/201302/28/3d20251a1b60350a93835fbb.jpg_256x160_e8f468b0.jpg',
+                    title: '日本樱花节',
+                    desc:
+                        '这里是日本一年一度的樱花节，有很多漂亮的小姐姐，很有意思哦'
+                },
+                {
+                    id: '0002',
+                    imgUrl:
+                        '//imgs.qunarzz.com/p/p29/201302/28/3d20251a1b60350a93835fbb.jpg_256x160_e8f468b0.jpg',
+                    title: '日本樱花节',
+                    desc:
+                        '这里是日本一年一度的樱花节，有很多漂亮的小姐姐，很有意思哦'
+                },
+                {
+                    id: '0003',
+                    imgUrl:
+                        '//imgs.qunarzz.com/p/p29/201302/28/3d20251a1b60350a93835fbb.jpg_256x160_e8f468b0.jpg',
+                    title: '日本樱花节',
+                    desc:
+                        '这里是日本一年一度的樱花节，有很多漂亮的小姐姐，很有意思哦'
+                },
+                {
+                    id: '0004',
+                    imgUrl:
+                        '//imgs.qunarzz.com/p/p29/201302/28/3d20251a1b60350a93835fbb.jpg_256x160_e8f468b0.jpg',
+                    title: '日本樱花节',
+                    desc:
+                        '这里是日本一年一度的樱花节，有很多漂亮的小姐姐，很有意思哦'
+                },
+                {
+                    id: '0005',
+                    imgUrl:
+                        '//imgs.qunarzz.com/p/p29/201302/28/3d20251a1b60350a93835fbb.jpg_256x160_e8f468b0.jpg',
+                    title: '日本樱花节',
+                    desc:
+                        '这里是日本一年一度的樱花节，有很多漂亮的小姐姐，很有意思哦'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+    .recommend-title
+        margin-top: .2rem
+        line-height: .8rem
+        background: #eee
+        text-indent: .2rem
+    .item
+        overflow: hidden
+        display: flex
+        height: 1.9rem
+        .item-img
+            width: 1.7rem
+            height: 1.7rem
+            padding: .1rem
+        .item-info
+            flex: 1
+            padding: .1rem
+            min-width: 0
+            .item-title
+                line-height: .54rem
+                font-size: .32rem
+                ellipsis()
+            .item-desc
+                line-height: .4rem
+                color: #ccc
+                ellipsis()
+            .item-button
+                line-height: .44rem
+                margin-top: .16rem
+                background: #ff9300
+                padding: 0 .2rem
+                border-radius: .06rem
+                color: #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>
+        <div class="recommend-title">周末去哪儿</div>
+        <ul>
+            <li
+                class="item border-bottom"
+                v-for="item in recommendList"
+                :key="item.id"
+            >
+                <div class="item-img-wrapper">
+                    <img class="item-img" :src="item.imgUrl" />
+                </div>
+                <div class="item-info">
+                    <p class="item-title">{{ item.title }}</p>
+                    <p class="item-desc">
+                        {{ item.desc }}
+                    </p>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeWeekend',
+    data () {
+        return {
+            recommendList: [
+                {
+                    id: '0001',
+                    imgUrl:
+                        '//tr-osdcp.qunarzz.com/tr-osd-tr-manager/img/5a11974656794b4715156501be10c1b7.jpg',
+                    title: '日本樱花节',
+                    desc:
+                        '这里是日本一年一度的樱花节，有很多漂亮的小姐姐，很有意思哦'
+                },
+                {
+                    id: '0002',
+                    imgUrl:
+                        '//tr-osdcp.qunarzz.com/tr-osd-tr-manager/img/5a11974656794b4715156501be10c1b7.jpg',
+                    title: '日本樱花节',
+                    desc:
+                        '这里是日本一年一度的樱花节，有很多漂亮的小姐姐，很有意思哦'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+    .recommend-title
+        margin-top: .2rem
+        line-height: .8rem
+        background: #eee
+        text-indent: .2rem
+    .item-img-wrapper
+        overflow: hidden
+        height: 0
+        padding-bottom: 33.9%
+    .item-img
+        width: 100%
+    .item-info
+        padding: .1rem
+        .item-title
+            line-height: .54rem
+            font-size: .32rem
+            ellipsis()
+        .item-desc
+            line-height: .4rem
+            color: #ccc
+            ellipsis()
+</style>
