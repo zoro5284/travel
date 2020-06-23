ffd19cf0cb539cbb07d30094552a5d95be88567a
zoro5284 <577270884@qq.com>
2020-06-23 13:38:08 +0800

    update vuex

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -18,6 +18,7 @@
 </template>
 
 <script>
+import {mapMutations} from 'vuex'
 import Bscroll from 'better-scroll'
 export default {
     name: 'CitySearch',
@@ -33,9 +34,10 @@ export default {
     },
     methods: {
         handleCityClick (city) {
-            this.$store.commit('changeCity', city)
+            this.changeCity(city)
             this.$router.push('/')
-        }
+        },
+        ...mapMutations(['changeCity'])
     },
     computed: {
         hadSearchData () {
